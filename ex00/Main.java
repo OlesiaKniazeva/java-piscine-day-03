4a9ce82814cf2a99fb59f478689c69e45f8c1e5c
Madeleine Yael <myael@student.21-school.ru>
2022-07-22 11:49:09 +0500

    add task ex00

---
+++ ex00/Main.java
@@ -0,0 +1,67 @@
+package ex00;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static int AMOUNT = 0;
+
+    public static void main(String[] args) {
+        checkArgument(args);
+        AMOUNT = returnNumFromStr(args[0]);
+
+        Thread human = Thread.currentThread();
+        human.setName("Human");
+
+        Runnable task = () -> {
+            for (int i = 0; i < AMOUNT; ++i) {
+                System.out.println(Thread.currentThread().getName());
+            }
+        };
+
+        Thread egg = new Thread(task, "Egg");
+        Thread hen = new Thread(task, "Hen");
+
+        egg.start();
+        hen.start();
+
+        try {
+            egg.join();
+            hen.join();
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
+
+        for (int i = 0; i < AMOUNT; ++i) {
+            System.out.println(human.getName());
+        }
+    }
+    private static int returnNumFromStr(String arg) {
+        String num = arg.substring(arg.indexOf('=') + 1);
+
+        if (new Scanner(num).hasNextInt()) {
+            return Integer.parseInt(num);
+        }
+
+        System.out.println("Wrong argument");
+        System.exit(1);
+        return 0;
+    }
+
+    private static void checkArgument(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Enter argument --count=NUM, " +
+                    "NUM is amount of times thread will display answer");
+            System.exit(1);
+        }
+        final String arg = "--count=";
+
+        if (!args[0].startsWith(arg) || args[0].length() <= arg.length()) {
+            System.out.println("Enter argument --count=num," +
+                    "num is amount of times thread will display answer");
+            System.exit(1);
+        }
+    }
+}
